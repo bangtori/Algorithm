8c0e2a469bf8bcf265add79ecec1a1e7f9ea4cfb
bangtori <58802345+bangtori@users.noreply.github.com>
2023-11-03 13:27:22 +0900

    [Bronze V] Title: A+B - 8, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.swift
@@ -0,0 +1,5 @@
+let t:Int = Int(readLine()!)!
+for i in 1...t {
+    let nums = readLine()!.split(separator: " ").map {Int($0)!}
+    print("Case #\(i): \(nums[0]) + \(nums[1]) = \(nums[0]+nums[1])")
+}
