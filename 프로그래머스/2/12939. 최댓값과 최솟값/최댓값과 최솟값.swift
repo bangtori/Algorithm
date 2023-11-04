ed1dc5159923ec98e8322928b411cd040ed68051
bangtori <58802345+bangtori@users.noreply.github.com>
2023-11-04 15:21:53 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.24 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.swift
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.swift
@@ -1,6 +1,4 @@
 func solution(_ s:String) -> String {
     let arr = s.split(separator: " ").map{ Int($0)! }
-    let max = arr.max()!
-    let min = arr.min()!
-    return "\(min) \(max)"
+    return "\(arr.min()!) \(arr.max()!)"
 }
