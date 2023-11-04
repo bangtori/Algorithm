50efd87e82e3a6cf5c76a811e79590a6452f5004
bangtori <58802345+bangtori@users.noreply.github.com>
2023-11-05 01:48:33 +0900

    [Silver V] Title: 너의 평점은, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.swift
@@ -0,0 +1,13 @@
+let scores: [String: Float] = ["A+": 4.5, "A0": 4.0, "B+": 3.5, "B0": 3.0, "C+": 2.5, "C0": 2.0, "D+": 1.5, "D0": 1.0, "F": 0.0]
+var hakjumSum: Float = 0.0
+var scoreSum: Float = 0.0
+for _ in 1...20 {
+    let input = readLine()!.split(separator: " ").map{String($0)}
+    if input[2] == "P" {
+        continue
+    }
+    let (hakjum, score) = (Float(input[1])!, scores[input[2]]!)
+    hakjumSum += hakjum
+    scoreSum += (hakjum * score)
+}
+print(scoreSum/hakjumSum)
