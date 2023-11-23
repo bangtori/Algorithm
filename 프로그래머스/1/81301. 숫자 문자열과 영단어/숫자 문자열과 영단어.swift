b05974a383853736ce51699be9006c36ca372656
방유빈 <58802345+bangtori@users.noreply.github.com>
2023-11-23 20:46:25 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.19 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.swift
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.swift
@@ -2,20 +2,9 @@ import Foundation
 
 func solution(_ s:String) -> Int {
     let wordDic: [String: String] = ["zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"]
-    var stringArr = Array(s)
-    var resultString = ""
-    var start = 0
-    for i in 0..<stringArr.count {
-        if stringArr[i].isNumber {
-            start = i + 1
-            resultString += String(stringArr[i])
-        } else {
-            let currentWord = stringArr[start...i].map{String($0)}.joined(separator: "")
-            if wordDic[currentWord] != nil {
-                resultString += wordDic[currentWord]!
-                start = i + 1
-            }
-        }
+    var s = s
+    for (word, number) in wordDic {
+        s = s.replacingOccurrences(of: word, with: String(number))
     }
-    return Int(resultString)!
+    return Int(s)!
 }
