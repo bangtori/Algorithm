579d98b6e3c1247510a1316626f269235311d9ae
방유빈 <58802345+bangtori@users.noreply.github.com>
2024-07-06 01:44:37 +0900

    [Silver V] Title: 큰 수 구성하기, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Silver/18511. 큰 수 구성하기/큰 수 구성하기.swift
+++ 백준/Silver/18511. 큰 수 구성하기/큰 수 구성하기.swift
@@ -1,35 +1,31 @@
 let input = readLine()!.split(separator: " ").map { Int($0)! }
 let (n, k) = (input[0], input[1])
-let kArr = readLine()!.split(separator: " ").map { Int($0)! }
-func dfs(currentNumber: String, digits: [String], maxNumber: Int) -> Int {
-    // 현재까지 만든 숫자가 maxNumber를 넘으면 중단
-    if Int(currentNumber)! > maxNumber {
+let kArr = readLine()!.split(separator: " ").map { String($0) }
+
+// currentNumber: 현재까지 만든 숫자
+func dfs(currentNumber: String) -> Int {
+    if Int(currentNumber)! > n {
         return 0
     }
     
     var maxResult = Int(currentNumber)!
     
-    for digit in digits {
+    for digit in kArr {
         let newNumber = currentNumber + digit
-        let result = dfs(currentNumber: newNumber, digits: digits, maxNumber: maxNumber)
+        let result = dfs(currentNumber: newNumber)
         maxResult = max(maxResult, result)
     }
     
     return maxResult
 }
 
-func findLargestNumber(N: Int, K: [Int]) -> Int {
-    let digits = K.map { String($0) }
-    let maxNumber = N
-    var maxResult = 0
-    
 
-    for digit in digits {
-        maxResult = max(maxResult, dfs(currentNumber: digit, digits: digits, maxNumber: maxNumber))
-    }
-    
-    return maxResult
+var maxResult = 0
+
+
+for digit in kArr {
+    maxResult = max(maxResult, dfs(currentNumber: digit))
 }
 
 
-print(findLargestNumber(N: n, K: kArr))
+print(maxResult)
