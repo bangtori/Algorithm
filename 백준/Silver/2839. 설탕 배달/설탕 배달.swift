47bae7484a3fa147e102a9302f06ddb466a51269
방유빈 <58802345+bangtori@users.noreply.github.com>
2024-07-24 09:16:42 +0900

    [Silver IV] Title: 설탕 배달, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.swift
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.swift
@@ -1,22 +1,18 @@
 var n = Int(readLine()!)!
-var result = 0
-if n % 5 == 0 {
-    result = n / 5
-    n = 0
-} else {
-    var bag = 0
-    while n > 0 {
-        n -= 3
-        result += 1
-        if n % 5 == 0 {
-            result += n/5
-            n = 0
-            break
-        }
+var count = 0
+
+while n > 0 {
+    if n % 5 == 0 {
+        count += (n / 5)
+        n = 0
+        break
     }
+    n -= 3
+    count += 1
 }
-if n != 0 {
-    print(-1)
+
+if n == 0 {
+    print(count)
 } else {
-    print(result)
+    print(-1)
 }
