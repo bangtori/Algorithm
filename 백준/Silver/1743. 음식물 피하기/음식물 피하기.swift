87ffd94247749de5c1c4282880087febf7199d29
방유빈 <58802345+bangtori@users.noreply.github.com>
2024-08-22 09:57:43 +0900

    [Silver I] Title: 음식물 피하기, Time: 12 ms, Memory: 69396 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.swift
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.swift
@@ -1,39 +1,46 @@
+typealias Position = (x: Int, y: Int)
 let input = readLine()!.split(separator: " ").map{Int($0)!}
 let (n, m, k) = (input[0], input[1], input[2])
-var map = [[Int]](repeating: [Int](repeating: 0, count: m), count: n)  // 음식물 1 / 없으면 0
+var map = [[Int]](repeating: [Int](repeating: 0, count: m), count: n)
+
 for _ in 1...k {
-    let position = readLine()!.split(separator: " ").map{Int($0)!}
-    let (y, x) = (position[0]-1, position[1]-1)
-    map[y][x] = 1
+    let pos = readLine()!.split(separator: " ").map{Int($0)!}
+    map[pos[0]-1][pos[1]-1] = 1
 }
-
-
 var visited = [[Bool]](repeating: [Bool](repeating: false, count: m), count: n)
-let moves = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-var maxCount = 0
-
-func dfs(_ x: Int, _ y: Int) -> Int {
-    var count = 1
-    visited[y][x] = true
+func bfs(_ start: Position) -> Int {
+    var queue = [Position]()
+    var index = 0
+    var count = 0
+    queue.append(start)
+    visited[start.y][start.x] = true
+    count += 1
+    let moves: [Position] = [(-1, 0), (1, 0), (0, -1), (0, 1)]
     
-    for move in moves {
-        let nx = x + move.0
-        let ny = y + move.1
+    
+    while queue.count > index {
+        let pos = queue[index]
+        index += 1
         
-        if 0..<m ~= nx && 0..<n ~= ny && map[ny][nx] == 1 && !visited[ny][nx] {
-            count += dfs(nx, ny)
+        for move in moves {
+            let next: Position = (pos.x+move.x, pos.y+move.y)
+            
+            if 0..<m ~= next.x && 0..<n ~= next.y && map[next.y][next.x] != 0 && visited[next.y][next.x] == false {
+                visited[next.y][next.x] = true
+                queue.append(next)
+                count += 1
+            }
         }
     }
-    
     return count
 }
 
+var maxResult = 0
 for x in 0..<m {
     for y in 0..<n {
-        if map[y][x] == 1 && !visited[y][x] {
-            let currentCount = dfs(x, y)
-            maxCount = max(maxCount, currentCount)
+        if map[y][x] != 0 && visited[y][x] == false {
+            maxResult = max(maxResult, bfs((x,y)))
         }
     }
 }
-print(maxCount)
+print(maxResult)
