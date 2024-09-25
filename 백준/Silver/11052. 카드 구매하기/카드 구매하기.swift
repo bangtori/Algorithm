4c8c602bd46aec5695d576e5e21b7bd3c94e26bb
방유빈 <58802345+bangtori@users.noreply.github.com>
2024-09-26 02:09:44 +0900

    [Silver I] Title: 카드 구매하기, Time: 32 ms, Memory: 76668 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.swift
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.swift
@@ -5,13 +5,9 @@ var dp = [[Int]](repeating: [Int](repeating: 0, count: n+1), count: n+1) // dp[y
 var maxDp = [Int](repeating: 0, count: n+1) // n개 뽑기 max 기록
 
 for y in 1...n {
-    for x in 1...n {
+    for x in 1...n { 
         if x > y { continue }
         dp[y][x] = prices[x-1] + maxDp[y-x]
-        if y % x == 0 {
-            let n = y / x
-            dp[y][x] = max(dp[y][x], prices[x-1] * n)
-        }
         maxDp[y] = max(maxDp[y], dp[y][x])
     }
 }
