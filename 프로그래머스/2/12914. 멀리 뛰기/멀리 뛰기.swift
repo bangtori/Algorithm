369060baf2f1928cac5cfaeaa03a9991e6d78025
방유빈 <58802345+bangtori@users.noreply.github.com>
2024-12-13 10:50:41 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.09 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.swift
@@ -0,0 +1,10 @@
+func solution(_ n:Int) -> Int {
+    var dp: [Int] = Array(repeating: 0, count: n+1)
+    if n < 3 { return n }
+    dp[1] = 1
+    dp[2] = 2
+    for i in 3...n {
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567
+    }
+    return dp[n]
+}
