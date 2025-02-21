7faed4f2b68a8967c158e33c0690476e47774a75
방유빈 <58802345+bangtori@users.noreply.github.com>
2025-02-21 10:18:51 +0900

    [level 3] Title: 섬 연결하기, Time: 0.49 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.swift
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.swift
@@ -1,48 +1,24 @@
-import Foundation
-
 func solution(_ n:Int, _ costs:[[Int]]) -> Int {
-    func bfs(graph: [Int: [Int]]) -> Bool {
-        var visited = [Bool](repeating: false, count: n)
-        var queue = [Int]()
-        visited[0] = true
-        graph[0]!.forEach {
-            queue.append($0)
-            visited[$0] = true
+    var parent = Array(0..<n)
+    let sortedCosts = costs.sorted{$0[2] < $1[2]}
+    
+    func find(_ x: Int) -> Int {
+        if parent[x] != x {
+            parent[x] = find(parent[x])
         }
-        var index = 0
-        while index < queue.count {
-            let node = queue[index]
-            index += 1
-            for next in graph[node] ?? [] {
-                if !visited[next] {
-                    visited[next] = true
-                    queue.append(next)
-                }
-            }
-        }
-        return visited.filter{!$0}.isEmpty
-    }
-    var graph = [Int: [Int]]()
-    for cost in costs {
-        graph[cost[0], default: []].append(cost[1])
-        graph[cost[1], default: []].append(cost[0])
-    }
-    let sortCosts = costs.sorted {
-        return $0[2] > $1[2]
+        return parent[x]
     }
     var sum = 0
-    for cost in sortCosts {
-        var newGraph = graph
-        if let index1 = newGraph[cost[0]]?.firstIndex(of: cost[1]), let index0 = newGraph[cost[1]]?.firstIndex(of: cost[0]) {
-            newGraph[cost[0]]?.remove(at: index1)
-            newGraph[cost[1]]?.remove(at: index0)
-        }
-        if bfs(graph: newGraph) {
-            // 제거 가능
-            graph = newGraph
-        } else {
+    sortedCosts.forEach { cost in
+        let rootA = find(cost[0])
+        let rootB = find(cost[1])
+        
+        if rootA != rootB {
+            // 다른 집합이므로 합치기
+            parent[rootA] = rootB
             sum += cost[2]
         }
     }
+    
     return sum
 }
